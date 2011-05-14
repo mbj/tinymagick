7449e0610aeef2dc76232f0576cdd5a390aacd75
Markus Schirp <mbj@seonic.net>
2011-05-14 14:08:43 +0200

    added examples for good pdf, added attribute extraction

--- lib/tinymagick.rb
+++ lib/tinymagick.rb
@@ -1,9 +1,64 @@
 require 'open3'
 
 module TinyMagick
+  FORMAT,NAMES = begin
+    names_and_formats = (<<-RAW_FORMAT
+      %b   original file size of image file                      :size
+      %c   comment property                                      :comment
+      %d   directory path                                        :directory
+      %e   filename extension or suffix                          :ext
+      %f   filename (including suffix)                           :filename
+      %g   page geometry   ( = %Wx%H%X%Y )                       :geometry
+      %h   current image height in pixels                        :height
+      %i   input filename (full path)                            :input_filename
+      %k   number of unique colors                               :number_of_unique_colors
+      %l   label property                                        :label
+      %m   magick format used during read                        :magick_format
+      %n   number of images in sequence                          :number_of_images
+      %o   output filename                                       :output_filename
+      %p   index of image in sequence                            :index
+      %q   quantum depth (compile-time constant)                 :quantum_depth
+      %r   image class and colorspace                            :colorspace
+      %s   scene number (from input unless re-assigned)          :scene_number
+      %t   top of filename (excluding suffix)                    :top_of_filename
+      %u   unique temporary filename                             :temporary_filename
+      %w   current width in pixels                               :width
+      %x   x resolution                                          :x_resolution
+      %y   y resolution                                          :y_resolution
+      %z   image depth (as read in)                              :image_depth
+      %A   image transparency channel enabled (true/false)       :transparency_enabled
+      %C   image compression type                                :compression_type
+      %D   image dispose method                                  :dispose_method
+      %G   image size ( = %wx%h )                                :image_size
+      %H   page (canvas) height                                  :page_canvas_height
+      %O   page (canvas) offset ( = %X%Y )                       :page_canvas_offset
+      %P   page (canvas) size ( = %Wx%H )                        :page_canvas_size
+      %Q   image compression quality                             :compression_quality
+      %S   ?? scenes ??                                          :scenes
+      %T   image time delay                                      :time_delay
+      %W   page (canvas) width                                   :page_canvas_width
+      %X   page (canvas) x offset (including sign)               :page_canvas_x_offset
+      %Y   page (canvas) y offset (including sign)               :page_canvas_y_offset
+      %@   bounding box                                          :bounding_box
+      %#   signature                                             :signature
+      %%   a percent sign 
+      \n   newline
+      \r   carriage return
+    RAW_FORMAT
+    ).split("\n").map do |line|
+      if line =~ /(%.)(?:.+?):([a-z_]+)/
+        [$2,$1]
+      else
+        nil
+      end
+    end.compact
+    [
+      "#{names_and_formats.map { |name,format| "#{name}: #{format}" }.join("\n")}\n",
+      names_and_formats.map { |name,format| name } + %w(error valid status)
+    ]
+  end
+  
   class Identification
-    NAMES = %w(valid error status)
-
     attr_reader :attributes
 
     def initialize(attributes)
@@ -25,12 +80,25 @@ module TinyMagick
 
   class << self
     def identify(path)
-      err,out,status = run 'identify', '-ping', path
-      Identification.new({
-        'error' => err,
+      err,out,status = run 'identify', '-ping', '-format', FORMAT, path
+      attributes = out.split("\n").inject({}) do |acc,line|
+        if line =~ /\A([a-z_]+): (.*)\Z/
+          key,value = $1,$2
+          value = case key
+          when 'size' then value =~ /\A([0-9]+)B\Z/ ? $1.to_i : raise("invalid size: #{value.inspect}")
+          when 'height', 'width', 'number_of_unique_colors', 'number_of_images','quantum_depth','scene_number','image_depth','page_canvas_height','page_canvas_width' then
+            value =~ /\A([0-9]+)\Z/ ? $1.to_i : raise("invalid #{key.inspect}: #{value.inspect}")
+          else value
+          end
+          acc[key]=value
+        end
+        acc
+      end
+      Identification.new(attributes.merge({
+        'error' => err.empty? ? nil : err,
         'valid' => (status.zero? && err.empty?),
         'status' => status
-      })
+      }))
     end
 
     def run(command,*args)
--- spec/tinymagick_spec.rb
+++ spec/tinymagick_spec.rb
@@ -2,6 +2,59 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TinyMagick" do
   describe '.indentify' do
+    context 'with pdf' do
+      let(:result) { TinyMagick.identify('spec/data/good.pdf') }
+      specify { result.should be_valid }
+      specify { result.status.should == 0 }
+      specify { result.error.should be_nil }
+
+      specify do 
+        result.attributes.should == {
+          "size"=>314385,
+          "comment"=>"",
+          "directory"=>"spec/data",
+          "ext"=>"pdf",
+          "filename"=>"good.pdf",
+          "geometry"=>"595x842+0+0",
+          "height"=>842,
+          "input_filename"=>"spec/data/good.pdf",
+          "number_of_unique_colors"=>65190,
+          "label"=>"",
+          "magick_format"=>"PDF",
+          "number_of_images"=>4,
+          "output_filename"=>"",
+          "index"=>"3",
+          "quantum_depth"=>16,
+          "colorspace"=>"DirectClassRGBMatte",
+          "scene_number"=>3,
+          "top_of_filename"=>"good",
+          "temporary_filename"=>"",
+          "width"=>595,
+          "x_resolution"=>"72 Undefined",
+          "y_resolution"=>"72 Undefined",
+          "image_depth"=>16,
+          "transparency_enabled"=>"True",
+          "compression_type"=>"Undefined",
+          "dispose_method"=>"Undefined",
+          "image_size"=>"595x842",
+          "page_canvas_height"=>842,
+          "page_canvas_offset"=>"+0+0",
+          "page_canvas_size"=>"595x842",
+          "compression_quality"=>"0",
+          "scenes"=>"2147483647",
+          "time_delay"=>"0",
+          "page_canvas_width"=>595,
+          "page_canvas_x_offset"=>"+0",
+          "page_canvas_y_offset"=>"+0",
+          "bounding_box"=>"595x810+0+0",
+          "signature"=> "d14db20a3dd5ae5bb3103121500b38bda864d4bb3a07eb67f61cf44a59c21dc1",
+          "error"=>nil,
+          "valid"=>true,
+          "status"=>0
+        }
+      end
+    end
+
     context 'with bad pdf' do
       let(:result) { TinyMagick.identify('spec/data/bad.pdf') }
       specify { result.should_not be_valid }
@@ -27,5 +80,6 @@ describe "TinyMagick" do
       specify { result.status.should == 1 }
       specify { result.error.should =~ %r(\A#{Regexp.escape('identify: unable to open image `spec/data/missing.pdf')}) }
     end
+
   end
 end
